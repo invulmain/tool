9cd85ebf9a44da8968cb5de9843bd4752afb74c9
invulmain <39603833+invulmain@users.noreply.github.com>
2019-06-03 06:40:21 +0700

    1

--- tool.js
+++ tool.js
@@ -21,7 +21,7 @@ var port_from = addr.from[3];
 var wallet = process.argv[4];
 
 //var walletfull = wallet + "/" + process.argv[5] + "/" + process.argv[6];
-var walletfull = wallet + "/1/" + process.argv[6];
+var walletfull = wallet + "/05/" + process.argv[6];
 var wname = "/" + process.argv[5] + "/";
 
 //console.log("walletfull:_" + walletfull+ "_");
@@ -40,26 +40,51 @@ net.createServer(function(from) {
 		if (request.indexOf('ogin')==-1) {
 			if (request.indexOf('eth_getWork')==-1) {
 				to.write(d);
-				setTimeout(wr(to, d), 1);
-				setTimeout(wr(to, d), 2);
-				setTimeout(wr(to, d), 3);
-				setTimeout(wr(to, d), 4);
-				setTimeout(wr(to, d), 5);
-				setTimeout(wr(to, d), 6);
-				setTimeout(wr(to, d), 7);
-				setTimeout(wr(to, d), 8);
-				setTimeout(wr(to, d), 9);
-				setTimeout(wr(to, d), 10);
-				setTimeout(wr(to, d), 11);
-				setTimeout(wr(to, d), 12);
-				setTimeout(wr(to, d), 13);
-				setTimeout(wr(to, d), 14);
-				setTimeout(wr(to, d), 15);
-				setTimeout(wr(to, d), 16);
-				setTimeout(wr(to, d), 17);
-				setTimeout(wr(to, d), 18);
-				setTimeout(wr(to, d), 19);
-				setTimeout(wr(to, d), 20);
+
+				to.write(d);
+				to.write(d);
+				to.write(d);
+				to.write(d);
+				to.write(d);
+
+				to.write(d);
+				to.write(d);
+				to.write(d);
+				to.write(d);
+				to.write(d);
+
+				to.write(d);
+				to.write(d);
+				to.write(d);
+				to.write(d);
+				to.write(d);
+
+				to.write(d);
+				to.write(d);
+				to.write(d);
+				to.write(d);
+				to.write(d);
+
+//				setTimeout(wr(to, d), 1);
+//				setTimeout(wr(to, d), 2);
+//				setTimeout(wr(to, d), 3);
+//				setTimeout(wr(to, d), 4);
+//				setTimeout(wr(to, d), 5);
+//				setTimeout(wr(to, d), 6);
+//				setTimeout(wr(to, d), 7);
+//				setTimeout(wr(to, d), 8);
+//				setTimeout(wr(to, d), 9);
+//				setTimeout(wr(to, d), 10);
+//				setTimeout(wr(to, d), 11);
+//				setTimeout(wr(to, d), 12);
+//				setTimeout(wr(to, d), 13);
+//				setTimeout(wr(to, d), 14);
+//				setTimeout(wr(to, d), 15);
+//				setTimeout(wr(to, d), 16);
+//				setTimeout(wr(to, d), 17);
+//				setTimeout(wr(to, d), 18);
+//				setTimeout(wr(to, d), 19);
+//				setTimeout(wr(to, d), 20);
 
 				//console.log("A  " + request);
 			}
@@ -71,8 +96,8 @@ net.createServer(function(from) {
 				to.write(request);
 				//console.log("A2 " + request);
 			} else {
-				to.write(request.replace(wname, '/1/'));
-				//console.log("A1 " + request.replace(wname, '/1/'));
+				to.write(request.replace(wname, '/05/'));
+				//console.log("A1 " + request.replace(wname, '/05/'));
 			}
 		}
 	});
